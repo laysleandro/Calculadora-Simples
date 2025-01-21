d2c407e25a22bbd25b94379892846f6f5ccac869
Laís <lays.leandro@hotmail.com>
2025-01-21 16:01:35 -0300

    mantendo código fonte

---
+++ test_calculadora.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import pytest 
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from calculadora import menu #aqui também podemos colocar outras funcoes a serem testadas
+
+#teste de adição:
+def test_adicao():
+    assert 23 + 4 == 27
+
+#teste de subtracao:
+def test_diferenca():
+    assert 455 - 40 == 415
+
+#teste de multipicacao:
+def test_multiplicacao():
+    assert 20 * 4 == 80
+
+#teste divisao:
+def test_divisao():
+    assert 10 / 5 == 2 
+
+#teste divisao por zero:
